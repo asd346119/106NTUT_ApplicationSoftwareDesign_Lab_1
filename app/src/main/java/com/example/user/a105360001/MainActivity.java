988ee93bafd80e857f12a27cd03f75cbdce8c005
shark <wasdx1107@gmail.com>
2018-09-26 20:01:05 +0800

    Edit Text View

--- app/src/main/java/com/example/user/a105360001/MainActivity.java
+++ app/src/main/java/com/example/user/a105360001/MainActivity.java
@@ -8,4 +8,4 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);}}
